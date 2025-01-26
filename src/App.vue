6f91c4d016d5d84c744638211011496a5457759c
asylbekuly <rabatkarabek8@gmail.com>
2025-01-26 23:17:46 +0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,118 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form action="" class="card" @submit.prevent="submitHandler">
+      <h1>Анкета на Vue разработчика</h1>
+      <!-- <div class="form-control" :class="{invalid: errors.name}">
+        <label for="name">Как тебя зовут?</label><br>
+        <input 
+        type="text" 
+        id="name"
+        placeholder="Введи имя"
+        v-model.trim="name">
+        <small v-if="errors.name">{{errors.name}}</small>
+      </div> -->
+      <app-input
+        :placeholder="'Введи имя'"
+        :error="errors.name"
+        label="Как тебя зовут?"
+        v-model="name"
+      ></app-input>
+
+      <div class="form-control">
+        <label for="age">Выбери Возраст</label><br>
+        <input 
+        type="number" 
+        id="age" 
+        max="70"
+        v-model.number="age"
+        >
+      </div>
+
+      <div class="form-control">
+        <label for="city">Твой город</label><br>
+        <select id="city" v-model="city">
+          <option value="ast">Astana</option>
+          <option value="shy">Shymkent</option>
+          <option value="ala">Almaty</option>
+          <option value="Krg">Karaganda</option>
+        </select>
+      </div>
+
+      <div class="form-checkbox">
+        <span><div class="label">Готов к переезду в Токио?</div></span>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" v-model="relocate" value="Yes">Да</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="radio" name="trip" v-model="relocate" value="No">Нет</label>
+        </div>
+      </div>
+      <div class="form-checkbox">
+        <span><div class="label">Что знаешь во Vue?</div></span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" value="vuex">Vuex</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" value="CLI">Vue CLI</label>
+        </div>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="skills" value="Router">Vue Router</label>
+        </div>
+      </div>
+      <button type="submit" class="btn primary">Отправить</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppInput from './AppInput.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      name: '',
+      age: 23,
+      city: 'shy',
+      relocate: null,
+      skills: [],
+      errors: {
+        name: null
+      }
+    }
+  },
+
+  components: { AppInput },
+
+  methods: {
+    formIsValid () {
+      let isValid = true
+      if (this.name.length === 0) {
+        this.errors.name = 'Name cant be null'
+        isValid = false
+      } else {
+        this.errors.name = null
+      }
+      return isValid
+    },
+    submitHandler () {
+      if (this.formIsValid()) {
+        console.group('Form Data')
+        console.log('Name:', this.name)
+        console.log('Age:', this.age)
+        console.log('City:', this.city)
+        console.log('Relocate:', this.relocate)
+        console.log('Skills:', this.skills)
+        console.groupEnd()
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .form-control small{
+    color:red;
+  }
+  .form-control.invalid input{
+    border-color:red;
+  }
 </style>
---
+++ src/AppInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="form-control" :class="{ invalid: error }">
+    <label :for="id">{{ label }}</label><br>
+    <input 
+      type="text" 
+      :id="id"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="onChange"
+    />
+    <small v-if="error">{{ error }}</small>
+  </div>
+</template> 
+
+<script>
+export default {
+  emits: ['update:modelValue'],
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    error: {
+      type: String,
+      default: ''
+    },
+    modelValue: String
+  },
+  methods: {
+    onChange (event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  },
+  data () {
+    return {
+      id: 'input-' + Math.random()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.form-control {
+  margin-bottom: 1rem;
+}
+.form-control.invalid input {
+  border: 1px solid red;
+}
+.form-control small {
+  color: red;
+}
+</style>
